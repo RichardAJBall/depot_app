4191dc6987f44bf16ff2d08bf7d42cd287fe5255
RichardAJBall <richardajball@hotmail.com>
2014-09-24 13:25:12 +0100

    Validation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    # adds update as member of this class
+    @update = {
+      title: 'abcdefg',
+      description: 'this is desc',
+      image_url: 'imagename.jpg',
+      price: 12.34
+    }
   end
 
   test "should get index" do
@@ -18,7 +25,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +42,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    patch :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    patch :update, id: @product, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
+	fixtures :products
+    test "products non empty" do
+    	product = Product.new
+    	assert product.invalid?
+    	assert product.errors[:title].any?
+    	assert product.errors[:description].any?
+    	assert product.errors[:image_url].any?
+    	assert product.errors[:price].any?
+	end
+test "products needs unique title" do
+    	product = Product.new(title: products(:ruby).title,
+    	description: "yyy",
+    	price: 1,
+    	image_url: "frd.gif"	)
+    	assert product.invalid?
+    	assert_equal ["has already been taken"], product.errors[:title]
+     end
+
   #   assert true
   # end
 end
